21812ef4a521602126b1037c34e0c0659d6cad26
l0st-idi0t <carlnod121@gmail.com>
2022-12-16 22:55:53 -0600

    this game is unplayable at the speed this ball is going

--- pong.cpp
+++ pong.cpp
@@ -10,6 +10,9 @@ int WIDTH = 640;
 int HEIGHT = 480;
 int paddleWidth = 10;
 int paddleHeight = 100;
+int ballSpeedX = 1;
+int ballSpeedY = 1;
+
 
 void logSDLError(std::ostream &os, const std::string &msg) {
     os << msg << " error: " << SDL_GetError() << std::endl;
@@ -34,10 +37,14 @@ void input(Rect &player) {
                         running = false;
                         break;
                     case SDLK_UP:
-                        player.setY(player.getY() - playerSpeedY);
+                        if (player.getY() > 0) {
+                            player.setY(player.getY() - playerSpeedY);
+                        }
                         break;
                     case SDLK_DOWN:
-                        player.setY(player.getY() + playerSpeedY);
+                        if (player.getY() < HEIGHT - paddleHeight) {
+                            player.setY(player.getY() + playerSpeedY);
+                        }
                         break;
                 }
                 break;
@@ -52,9 +59,22 @@ void render(std::vector<Rect> rects, SDL_Renderer *renderer) {
     for (Rect rect : rects) {
         rect.drawRect(renderer);
     }
-    
+
     SDL_RenderPresent(renderer);
 }
+
+void ballMove(Rect &ball) {
+    if (ball.getX() > WIDTH - 10 || ball.getX() < 0) {
+        ballSpeedX *= -1;
+    }
+    ball.setX(ball.getX() + ballSpeedX);
+
+    if (ball.getY() > HEIGHT - 10 || ball.getY() < 0) {
+        ballSpeedY *= -1;
+    }
+    ball.setY(ball.getY() + ballSpeedY);
+}
+
 
 int main(int argc, char *argv[]) {
     //window to render to
@@ -75,10 +95,12 @@ int main(int argc, char *argv[]) {
 
     Rect l_paddle(0, HEIGHT/2, paddleWidth, paddleHeight);
     Rect r_paddle(WIDTH - paddleWidth, HEIGHT/2, paddleWidth, paddleHeight);
+    Rect ball(WIDTH/2, HEIGHT/2, 10, 10);
 
     while (running) {
         input(l_paddle);
-        render({l_paddle, r_paddle}, renderer);
+        ballMove(ball);
+        render({l_paddle, r_paddle, ball}, renderer);
     }
 
     cleanup(window, renderer);
